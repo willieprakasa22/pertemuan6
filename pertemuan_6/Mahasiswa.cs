5eb19ce8e43ebbf671c6a2d53d3c0d03a70af4a9
hanafihp <fattaahherdansyah@students.amikom.ac.id>
2023-07-11 20:39:46 +0700

    first commit

---
+++ pertemuan_6/Mahasiswa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace pertemuan_6
+{
+	public class Mahasiswa
+	{
+        private string _namaDepan;
+        private string _namaBelakang;
+
+        public string Nim { get; set; }
+        public string NamaDepan
+        {
+            get
+            {
+                return _namaDepan;
+            }
+            set
+            {
+                _namaDepan = value;
+            }
+        }
+
+        public string NamaBelakang
+        {
+            get
+            {
+                return _namaBelakang;
+            }
+            set
+            {
+                _namaBelakang = value;
+            }
+        }
+
+
+        public string NamaLengkap
+        {
+            get
+            {
+                return _namaDepan + ' ' + _namaBelakang;
+            }
+
+        }
+    }
+} 
+
---
+++ pertemuan_6/Program.cs
@@ -0,0 +1,16 @@
+﻿using pertemuan_6;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.NamaDepan = "Niken";
+        mhs1.NamaBelakang = "Lismiati";
+        mhs1.Nim = "22.11.4797";
+
+        Console.WriteLine(mhs1.NamaLengkap);
+        Console.WriteLine(mhs1.Nim);
+        Console.ReadKey();
+    }
+}
